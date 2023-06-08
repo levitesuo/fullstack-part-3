1b356442774b3d7f81b81dd001f32e3ff3c757f3
levitesuo <leevi.suotula@gmail.com>
2023-06-08 18:58:10 +0300

    Added port variability

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-var morgan = require('morgan')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :req[Content-Length] :status - :total-time ms :type'))
 
@@ -90,7 +91,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server persons is running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
