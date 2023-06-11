33e0d566acc7f43db4f99cb650e4f77e46d08bf4
levitesuo <leevi.suotula@gmail.com>
2023-06-11 22:15:04 +0300

    3c done

--- index.js
+++ index.js
@@ -8,40 +8,22 @@ const Person = require('./models/person')
 
 morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
 
+
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
+
 app.use(morgan('tiny'), )
 app.use(morgan(':method :url :req[Content-Length] :status - :total-time ms :type', {
   skip: function (req, res) {return req.method !== "POST"}
 }))
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static('build'))
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-const generateId = () => {
-  return Math.floor(Math.random()*10**6)
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformed id'})
+  }
+  next(error)
 }
 
 app.post('/api/persons', (request, response) => {
@@ -50,12 +32,6 @@ app.post('/api/persons', (request, response) => {
   if (!(body.number && body.name)) {
     return response.status(400).json({
       error: 'invalidArgument: Entries must have name and number'
-    })
-  }
-
-  if (persons.find(p => p.name === body.name)) {
-    return response.status(400).json({
-      error: 'invalidArgument: Name alredy exists'
     })
   }
 
@@ -69,35 +45,62 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-/* app.get('/', (request, response) => {
+app.get('/api/info',async (request, response) => {
+  const dbSize = await Person.countDocuments({})
   const landingPage = 
     `<br>
-      <p>Phonebook has info for ${persons.length} people</p>
-      <p>${Date(Date.now()).toString()}</p>
+      <h3>On this page is info about the api and instructions on how to use it.</h3>
+      <p>Phonebook has the info of ${dbSize} people</p>
+      <p>on ${Date(Date.now()).toString()}</p>
+      <h4>instructions </h4>
+      <p>The api contains numbers and names of people. This data is stored in an array of json objects.</p>
+      <p>Each object has a unique id, a name and a number. </p>
+      <p>the format is</p>
+      <p>{</p>
+      <p>id: nkkaoldjffhsdf1923,</p>
+      <p>name: Eric Example,</p>
+      <p>number: 040-123438123</p>
+      <p> }</p>
     </br>`
   response.send(landingPage)
 })
- */
+
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findById(id).then(person => {
     response.json(person)
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findByIdAndRemove(id)
   .then(result => {
     response.status(204).end()
   })
   .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
